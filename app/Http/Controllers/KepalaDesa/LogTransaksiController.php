c1b5334829a1df56b2ce09ba3373826019a0896d
heisoyyy <heikelqushoyi@gmail.com>
2026-04-27 08:47:32 +0700

    #1

---
+++ app/Http/Controllers/KepalaDesa/LogTransaksiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\KepalaDesa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogTransaksiController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Pengurus/SaldoAwalController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Pengurus;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SaldoAwalController extends Controller
+{
+    //
+}
